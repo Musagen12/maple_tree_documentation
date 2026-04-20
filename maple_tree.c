250067188226517a516926d187a8d200b7d7341c
Musagen12 <colonelmkar@outlook.com>
2026-04-20 11:32:12 +0300

    mas_wr_new_end() function purpose

--- maple_tree.c
+++ maple_tree.c
@@ -4301,7 +4301,7 @@ static inline void mas_wr_end_piv(struct ma_wr_state *wr_mas)
 // Study this for potential bugs since the function determines the store type based on a simple arithmetic calculation
 
 
-
+// The function returns the total slot count the node will have after the write
 static inline unsigned char mas_wr_new_end(struct ma_wr_state *wr_mas)
 {
 	// Extract the "ma_state" from the "ma_wr_state"
